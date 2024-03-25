b3ba3276097fedd39fc704b89e90a336bd7bafbe
matthew <mashh@live.co.uk>
2024-03-25 19:38:38 +0000

    playRound function created, checks whether player has won or lost round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,4 +3,30 @@ function getComputerChoice() {
   return choice[Math.floor(Math.random() * 3)];
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+  const playersChoice = playerSelection.toLowerCase();
+  if (playersChoice === computerSelection) {
+    return `It's a tie!`;
+  }
+  if (playersChoice === 'rock') {
+    if (computerSelection !== 'Paper') {
+      return `You Win! Rock beats ${computerSelection}`;
+    } else {
+      return `You Lose! ${computerSelection} beats Rock`;
+    }
+  }
+  if (playersChoice === 'paper') {
+    if (computerSelection !== 'Scissors') {
+        return `You Win! Paper beats ${computerSelection}`
+    } else {
+        return `You Lose! ${computerSelection} beats Paper`
+    }
+  }
+  if (playersChoice === 'scissors') {
+    if (computerSelection !== 'Rock') {
+        return `You Win! Scissors beat ${computerSelection}`
+    } else {
+        return `You Lose! ${computerSelection} beat Scissors`
+    }
+  }
+}
