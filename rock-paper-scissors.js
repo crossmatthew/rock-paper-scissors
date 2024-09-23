be3e9c6b40b9483e90b6e4c180290b83907e77ce
matthew <mashh@live.co.uk>
2024-09-23 19:35:56 +0100

    removed all returns.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,49 +6,43 @@ function getComputerChoice() {
   return choice[Math.floor(Math.random() * 3)];
 };
 
-function playGame(numOfRounds = 5) {
+function playGame(numOfRounds = 0) {
   let scoreboard = {
     playerWins: 0,
     computerWins: 0,
     ties: 0
   }
   function playRound(playerSelection, computerSelection) {
+    numOfRounds++;
     if (playerSelection === computerSelection) {
-      console.log(`It's a tie!`);
       scoreboard.ties++;
-      return `It's a tie!`;
+      console.log(`It's a tie!`);
     }
     if (playerSelection === "Rock") {
       if (computerSelection !== "Paper") {
-        console.log(`You Win! Rock beats ${computerSelection}`);
         scoreboard.playerWins++;
-        return `You Win! Rock beats ${computerSelection}`;
+        console.log(`You Win! Rock beats ${computerSelection}`);
       } else {
-        console.log(`You Lose! ${computerSelection} beats Rock`);
         scoreboard.computerWins++;
-        return `You Lose! ${computerSelection} beats Rock`;
+        console.log(`You Lose! ${computerSelection} beats Rock`);
       }
     }
     if (playerSelection === "Paper") {
       if (computerSelection !== "Scissors") {
-        console.log(`You Win! Paper beats ${computerSelection}`);
         scoreboard.playerWins++;
-        return `You Win! Paper beats ${computerSelection}`;
+        console.log(`You Win! Paper beats ${computerSelection}`);
       } else {
-        console.log(`You Lose! ${computerSelection} beats Paper`);
         scoreboard.computerWins++;
-        return `You Lose! ${computerSelection} beats Paper`;
+        console.log(`You Lose! ${computerSelection} beats Paper`);
       }
     }
     if (playerSelection === "Scissors") {
       if (computerSelection !== "Rock") {
-        console.log(`You Win! Scissors beat ${computerSelection}`);
         scoreboard.playerWins++;
-        return `You Win! Scissors beat ${computerSelection}`;
+        console.log(`You Win! Scissors beat ${computerSelection}`);
       } else {
-        console.log(`You Lose! ${computerSelection} beat Scissors`);
         scoreboard.computerWins++
-        return `You Lose! ${computerSelection} beat Scissors`;
+        console.log(`You Lose! ${computerSelection} beat Scissors`);
       } 
     }
   }
@@ -60,15 +54,18 @@ function playGame(numOfRounds = 5) {
   if (scoreboard.computerWins > scoreboard.playerWins) {
     console.log(" Computer Wins!");
   }
-  if (scoreboard.playerWins === scoreboard.computerWins) {
-    console.log('Tied! No Winner!');
+  if (
+    scoreboard.playerWins === scoreboard.computerWins &&
+    scoreboard.playerWins !== 0 &&
+    scoreboard.computerWins !== 0) {
+    console.log("Tied! No Winner!");
   }
 }
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     playRound(button.id, getComputerChoice());
       console.log(
-        `Player's wins: ${scoreboard.playerWins}, Computer's wins: ${scoreboard.computerWins}, Ties: ${scoreboard.ties}, Rounds Played: ${numOfRounds} ##### `
+        `Player's wins: ${scoreboard.playerWins}, Computer's wins: ${scoreboard.computerWins}, Ties: ${scoreboard.ties}, Rounds Played: ${numOfRounds}`
       );
   });
 });
